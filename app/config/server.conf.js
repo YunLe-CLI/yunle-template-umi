5b38f9ad8426320f660ef517ba05cf7f4574c1ea
hexiao <hexiao@sobey.com>
2016-10-31 23:41:14 +0800

    commit@0.0.1

---
+++ app/config/server.conf.js
@@ -0,0 +1,28 @@
+'use strict';
+const path = require('path');
+
+const NODE_ENV = process.env.NODE_ENV === 'production';
+let host = 'localhost';
+if (NODE_ENV) {
+  host = 'usersMongo';
+}
+
+module.exports = {
+  env: !NODE_ENV,
+  port: NODE_ENV ? 3000 : 3000,
+  proxy: {
+    host: 'https://www.baidu.com/',
+    path: '/s',
+  },
+  render: {
+    root: path.join(__dirname, '../view'),
+    layout: false,
+    viewExt: 'html',
+    cache: NODE_ENV,
+    debug: !NODE_ENV,
+    delimiter: '%',
+  },
+  mongodb: `mongodb://${host}:27017/yunle`,
+  redis: `redis://${host}:6379`,
+  amqp: 'amqp://localhost:5672',
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './less/base.less';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.querySelector('#root')
+);
