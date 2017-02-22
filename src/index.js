187f1384c57bbbff26bc50fa3d179bc765fa67bd
何潇 <hexiao@hexiaodeMacBook-Pro.local>
2017-02-22 10:35:20 +0800

    use yarn

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import 'babel-polyfill';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Root from './containers';
-
 import './index.html';
+
 ReactDOM.render(
   <Root />,
-  document.getElementById('root')
+  document.getElementById('root'),
+  () => {
+    let index = 1;
+    const time = setInterval(() => {
+      const loading = document.getElementById('global-loading-wrap');
+      if (!loading) return;
+      if (index >= 0) {
+        index -= 0.1;
+        loading.style.opacity = index;
+      } else {
+        clearTimeout(time);
+        loading.style.display = 'none';
+      }
+    }, 100);
+  },
 );
